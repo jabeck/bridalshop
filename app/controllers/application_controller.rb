82c393c0869fb2adc037be7de0c365bf5eed12b4
Jabeck <jabeck@nmu.edu>
2010-05-12 23:21:50 -0400

    Fixing error pages again.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,14 @@ class ApplicationController < ActionController::Base
   # Scrub sensitive parameters from your log
   filter_parameter_logging :password
   
+  def rescue_action_in_public(exception)
+    # do something based on exception
+    errors = [ActionController::RoutingError, ActionController::UnknownAction, ActiveRecord::RecordNotFound]
+    message = exception.backtrace.join("\n") unless exception
+    if errors.include?(exception)
+      render :file => "#{RAILS_ROOT}/public/404.html", :layout => false, :status => 404
+    else
+      render :file => "#{RAILS_ROOT}/public/500.html", :layout => false, :status => 500
+    end
+  end  
 end
