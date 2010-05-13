8cd0bde1896a40e0e9ac72d2f6c2aa3db529d2b1
Jabeck <jabeck@nmu.edu>
2010-05-12 20:21:12 -0400

    Fixed more things

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.root :controller => 'shop'
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
   map.register '/register', :controller => 'admins', :action => 'create'
